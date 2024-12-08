4b1bbf3adebf25160761b19e0a82b841b02e8fbb
TatianaYudina87 <tanyamaz8@gmail.com>
2024-12-08 17:21:31 +0300

    Add files via upload

---
+++ module_1_basic_data_structures.py
@@ -0,0 +1,8 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted_students = sorted(list(students))
+average_grades = {}
+for i, student in enumerate(sorted_students):
+    average_grade = sum(grades[i]) / len(grades[i])
+    average_grades[student] = average_grade
+print(average_grades)
